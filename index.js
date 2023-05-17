2884aa045fd24fd8ab16baeb1e1db459fd6b7f81
ITP <RayhanUddinFarhad11@gmail.com>
2023-05-17 22:24:39 +0600

    server side  setup completed

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use (express.json());
+app.use (cors())
+
+
+app.get('/', (req, res) => {
+
+
+    res.send ("Toys is here!");
+})
+
+app.listen (port, (req, res) => {
+
+    console.log ("listening on port " + port);
+})
